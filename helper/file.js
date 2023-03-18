709183565898e1802fb039cdcd1eb34f45d00a46
LeeNux <lc7457@qq.com>
2023-03-18 11:13:35 +0800

    增加两个文件类型方法

--- helper/file.js
+++ helper/file.js
@@ -1,12 +1,11 @@
 /**
  * 将文件转换为二进制并下载
- * @param {*} content 需要保存的内容
+ * @param {File} file
  * @param {string} name 文件名
- * @param {object} [opt] 文件保存选项
  * @returns {boolean}
  */
-export function toDownload(content, name, opt = {}) {
-    let url = URL.createObjectURL(new Blob([content], opt));
+export const toDownload = (file, name) => {
+    let url = URL.createObjectURL(file);
     let link = document.createElement("a");
     link.style.display = "none";
     link.href = url;
@@ -17,4 +16,40 @@ export function toDownload(content, name, opt = {}) {
     document.body.removeChild(link); // 下载完成移除元素
     URL.revokeObjectURL(url); // 释放掉blob对象
     return true
+}
+
+/**
+ * 将Base64图片转为File
+ * @param {string} dataURL 图像Base64
+ * @param {string} filename 文件名，不需要后缀
+ * @returns {File}
+ */
+export const dataURLImageToFile = (dataURL, filename) => {
+    const arr = dataURL.split(',')
+    const mime = arr[0].match(/:(.*?);/)[1]
+    const suffix = mime.split('/')[1]
+    const bStr = atob(arr[1])
+    const options = {type: mime}
+    let n = bStr.length
+    const u8arr = new Uint8Array(n)
+    while (n--) {
+        u8arr[n] = bStr.charCodeAt(n)
+    }
+    return new File([u8arr], `${filename}.${suffix}`, options)
+}
+
+/**
+ * 字符保存为文件
+ * @param content 内容
+ * @param {string} filename 文件名，不需要后缀
+ * @param mime
+ * @returns {File}
+ */
+export const stringToFile = (content, filename = 'file', mime = "application/json") => {
+    const suffix = mime.split('/')[1]
+    const options = {
+        type: mime,
+        lastModified: Date.now()
+    }
+    return new File([content], `${filename}.${suffix}`, options);
 }
